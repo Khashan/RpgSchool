c1bb154877b02208f523be1e2be1bb100487e2d6
whateverguys <thomasmainguy12@gmail.com>
2019-09-08 22:11:54 -0400

    delete assest gothic whatever added cinemachine/player basic controller to test camera / lvl1 done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int m_CurrentCol = 0;
+    private int m_CurrentRng = 0;
+    
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            gameObject.transform.position += new Vector3(-1,0,0);
+            --m_CurrentRng;
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(1,0,0);
+            ++m_CurrentRng;
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            gameObject.transform.position += new Vector3(0,-1,0);
+            --m_CurrentCol;
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            gameObject.transform.position += new Vector3(0,1,0);
+            ++m_CurrentCol;
+        }
+    }
+}

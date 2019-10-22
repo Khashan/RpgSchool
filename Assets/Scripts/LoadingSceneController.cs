ca7a624213bd43936bb3588081c9d75615d34e31
kyllthor <wowdavidpoirier@hotmail.fr>
2019-10-22 18:40:18 -0400

    - ajout de 2 new scene   === LoadingScene                          === MainMenu
    il sont deja linker au project il faut juste les ajouter dans le build

---
+++ Assets/Scripts/LoadingSceneController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingSceneController : MonoBehaviour
+{
+    [SerializeField]
+    private float m_CurrentTime = 0f;
+    [SerializeField]
+    private float m_Delay = 20f;
+    [SerializeField]
+    private int m_RandomDelay;
+
+    [SerializeField]
+    private Slider m_LoadingBar;
+
+    void Update()
+    {
+        m_CurrentTime += m_CurrentTime + Time.deltaTime;
+
+        m_Delay = Random.Range(0, 8);
+        if (m_CurrentTime > m_Delay)
+        {
+            m_Delay = Random.Range(20, 80);
+            m_CurrentTime = 0;
+            m_RandomDelay = Random.Range(0, 10);
+            m_LoadingBar.value += m_RandomDelay;
+
+            if(m_LoadingBar.value >= 100)
+            {
+                LevelManager.Instance.ChangeLevel("ThomasScene", true, 1f);
+            }
+        }
+
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void loadScene(string a_Scene)
+    {
+        LevelManager.Instance.ChangeLevel(a_Scene, true, 0);
+    }
+}

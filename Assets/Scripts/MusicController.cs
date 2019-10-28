0e08fae75318d566dd31ef39e0801b9d8331cef3
Khashan <simonandersonl@hotmail.com>
2019-10-28 09:46:04 -0400

    Added AudioManager  Main Menu Music & Loading Screen Music
    Modified Main Menu Scene
     - Added Grid Layout
     - Added Button "Continue"
     - Added Music Controller
    
    Modified SceneLoader
     - Added Music Controller

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip m_Music;
+
+    private void Awake()
+    {
+        if (m_Music != null)
+        {
+            AudioManager.Instance.PlayMusic(m_Music);
+        }
+    }
+}

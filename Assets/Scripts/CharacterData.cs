59230886103f0c1032678240bbf2b06f11e95366
MTurcotte96 <1830182@bart.ca>
2019-10-29 15:23:29 -0400

    trying to pull :)

---
+++ Assets/Scripts/CharacterData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "ScriptableObject/CharacterData", order =2)]
+public class CharacterData : ScriptableObject
+{
+    public int m_MaxHealth = 100;
+    public int m_CurrentHealth = 100;
+    public int m_AttackDamage = 40;
+    public GameObject m_CharacterVisualPrefab;
+}
---
+++ Assets/Scripts/Managers/CombatManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : Singleton<CombatManager>
+{
+    [SerializeField]
+    private List<GameObject> m_ListBackground = new List<GameObject>();
+
+    //[SerializeField]
+    //private List<>
+
+    private List<object> m_PlayerTeam = new List<object>();
+
+    private List<object> m_EnnemyTeam = new List<object>();
+
+    public void CombatSetup()
+    {
+
+    }
+    
+}

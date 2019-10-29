2c8c43bed2307c04fb2e0da93c9ff66fbaab8d54
MTurcotte96 <1830182@bart.ca>
2019-10-29 19:42:06 -0400

    Added Animations for the friendly dudes, added scriptableobjects for characters, started the combatmanager and combatcontroller for the CombatScene

---
+++ Assets/Scripts/Controller/CombatController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatController : MonoBehaviour
+{
+    [SerializeField]
+    private List<CharacterData> m_FriendlyList = new List<CharacterData>();
+    [SerializeField]
+    private List<Vector2> m_FriendlyIdlePositions = new List<Vector2>();
+    private List<Vector2> m_FriendlyAttackingPositions = new List<Vector2>();
+
+
+    [SerializeField]
+    private List<CharacterData> m_EnnemyList = new List<CharacterData>();
+    [SerializeField]
+    private List<Vector2> m_EnnemyIdlePositions = new List<Vector2>();
+    private List<Vector2> m_EnnemyAttackingPositions = new List<Vector2>();
+
+    [SerializeField]
+    private float m_TravelLerpDuration = 1.5f;
+
+
+
+    private bool m_IsFriendlyTurn = true;
+
+
+    private void Start()
+    {
+        CombatManager.Instance.InitEnnemyTeam();
+        CombatManager.Instance.CombatSetup(m_FriendlyList, m_EnnemyList);
+        m_EnnemyAttackingPositions[0] = new Vector2(m_FriendlyIdlePositions[0].x + 2, m_FriendlyIdlePositions[0].y);
+        m_EnnemyAttackingPositions[1] = new Vector2(m_FriendlyIdlePositions[1].x + 2, m_FriendlyIdlePositions[1].y);
+        m_EnnemyAttackingPositions[2] = new Vector2(m_FriendlyIdlePositions[2].x + 2, m_FriendlyIdlePositions[2].y);
+
+        m_FriendlyAttackingPositions[0] = new Vector2(m_EnnemyIdlePositions[0].x - 2, m_EnnemyIdlePositions[0].y);
+        m_FriendlyAttackingPositions[1] = new Vector2(m_EnnemyIdlePositions[1].x - 2, m_EnnemyIdlePositions[1].y);
+        m_FriendlyAttackingPositions[2] = new Vector2(m_EnnemyIdlePositions[2].x - 2, m_EnnemyIdlePositions[2].y);
+    }
+
+    private void CombatOver()
+    {
+        m_EnnemyList.Clear();
+        m_FriendlyList.Clear();
+    }
+
+
+    private void Update()
+    {
+
+    }
+
+    private void Attack(CharacterData aAttackingCharacter, CharacterData aAttackedCharacter)
+    {
+        //float currentlerp = Vector2.Lerp()
+    }
+}
--- Assets/Scripts/Managers/CombatManager.cs
+++ Assets/Scripts/Managers/CombatManager.cs
@@ -3,21 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CombatManager : Singleton<CombatManager>
 {
     [SerializeField]
     private List<GameObject> m_ListBackground = new List<GameObject>();
+    [SerializeField]
+    private List<CharacterData> m_PossibleEnnemyList = new List<CharacterData>();
 
     //[SerializeField]
     //private List<>
 
-    private List<object> m_PlayerTeam = new List<object>();
 
-    private List<object> m_EnnemyTeam = new List<object>();
 
-    public void CombatSetup()
+
+    private List<CharacterData> m_PlayerTeam = new List<CharacterData>();
+
+    private List<CharacterData> m_EnnemyTeam = new List<CharacterData>();
+
+    public void InitEnnemyTeam()
     {
+        //adding 3 random ennemies from the possible ennemies list
+        //this will get called at the start of the combat scene
+        for(int i = 0; i <= 3; i++)
+        {
+            int rand = (int)UnityEngine.Random.Range(0, m_PossibleEnnemyList.Count);
+            m_EnnemyTeam.Add(m_PossibleEnnemyList[rand]);
+        }
+    }
 
+    public void CombatSetup(List<CharacterData> aFriendlyList, List<CharacterData> aEnnemyList)
+    {
+        //this sets the combatcontroller's list to the current playerteam and ennemy team
+        //this will get called right after we initialize the ennemies using InitEnnemyTeam()
+        aFriendlyList = m_PlayerTeam;
+        aEnnemyList = m_EnnemyTeam;
+        m_EnnemyTeam.Clear();
     }
     
 }

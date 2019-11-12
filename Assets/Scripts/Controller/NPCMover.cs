87fffae4e94ef2f4b733f353a547f05211f00a9e
whateverguys <thomasmainguy2@gmail.com>
2019-11-12 15:32:16 -0500

    Implement NPC movement and PlayerController Vector3 change

--- Assets/Scripts/Controller/NPCMover.cs
+++ Assets/Scripts/Controller/NPCMover.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class NPCMover : MonoBehaviour
 {
     [SerializeField]
     private PlayerData m_Data;
     [SerializeField]
-    private float m_Speed;
+    private float m_Speed = 1f;
     [SerializeField]
     private float m_Hp;
 
@@ -18,7 +18,12 @@ public class PlayerController : MonoBehaviour
     private Vector2 m_InitialPos;
     private Vector2 m_WantedPos;
     private bool m_IsInCombat = false;
-    private Vector3 m_Movement = new Vector3();
+
+    private Vector3 m_Right = new Vector3(1,0,0);
+    private Vector3 m_Left = new Vector3(-1,0,0);
+    private Vector3 m_Up = new Vector3(0,1,0);
+    private Vector3 m_Down = new Vector3(1,-1,0);
+
 
     public void SetBool(bool aBool)
     {
@@ -29,19 +34,6 @@ public class PlayerController : MonoBehaviour
         return m_IsInCombat;
     }
     
-    [SerializeField]
-    private int m_DistanceTravelled = 0;
-
-    public void SetDistanceTravelled(int aDistance)
-    {
-        m_DistanceTravelled = aDistance;
-    }
-
-    private void Awake()
-    {
-        GameManager.Instance.PlayerController(this);
-    }
-
     private void Start()
     {
         m_Hp = m_Data.m_Hp;
@@ -51,26 +43,29 @@ public class PlayerController : MonoBehaviour
     {
         if(!m_IsInCombat)
         {
-            float InputsX = Input.GetAxisRaw("Horizontal");
-            float InputsY = Input.GetAxisRaw("Vertical");
             if(!m_IsMoving)
             {
-                if (InputsX != 0)
+                System.Random rand = new System.Random();
+                int tRandom1 = rand.Next(-2, 2);
+                System.Random tRand = new System.Random();
+                int tRandom2 = rand.Next(-2, 2);
+
+                if (tRandom1 == 1)
                 {
-                    m_IsMoving = true;
-                    m_PercentageCompletion = 0f;
-                    m_InitialPos = transform.position;
-                    m_Movement.x = InputsX;
-                    m_WantedPos = transform.position + m_Movement;
+                    MoveNPC(m_Right);
+                }
+                else if(tRandom1 == -1)
+                {
+                    MoveNPC(m_Left);
                 }
 
-                else if(InputsY != 0)
+                if (tRandom2 == 1)
                 {
-                    m_IsMoving = true;
-                    m_PercentageCompletion = 0f;
-                    m_InitialPos = transform.position;
-                    m_Movement.y = InputsY;
-                    m_WantedPos = transform.position + m_Movement;
+                    MoveNPC(m_Up);
+                }
+                else if(tRandom2 == -1)
+                {
+                    MoveNPC(m_Down);
                 }
             }
 
@@ -83,12 +78,17 @@ public class PlayerController : MonoBehaviour
 
                 if (m_PercentageCompletion >= 1)
                 {
-                    m_DistanceTravelled++;
-                    GameManager.Instance.GetPlayerDistance(m_DistanceTravelled);
-                    m_Movement = Vector3.zero;
                     m_IsMoving = false;
                 }
             }
         }
+    }
+
+    private void MoveNPC(Vector3 aOrientation)
+    {
+        m_IsMoving = true;
+        m_PercentageCompletion = 0f;
+        m_InitialPos = transform.position;
+        m_WantedPos = transform.position + aOrientation;
     }
 }

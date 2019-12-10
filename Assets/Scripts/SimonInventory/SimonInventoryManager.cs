b1307ce42f25a202488f35ee01b9308886941cdf
Khashan <simonandersonl@hotmail.com>
2019-12-10 16:17:21 -0500

    Fixed CustomWindow Error where the gameObject, somewhat was disable.

--- Assets/Scripts/SimonInventory/SimonInventoryManager.cs
+++ Assets/Scripts/SimonInventory/SimonInventoryManager.cs
@@ -10,5 +10,51 @@ using UnityEngine;
 
 public class SimonInventoryManager : MonoBehaviour
 {
-    private List<SimonItemData> m_Items = new List<SimonItemData>();
+    public struct Item
+    {
+        public SimonItemData m_ItemData;
+        public int m_Quantity;
+
+        public void AddQuantity(int aAmount)
+        {
+            m_Quantity += aAmount;
+
+            if(m_Quantity > m_ItemData.StackSize)
+            {
+                m_Quantity = m_ItemData.StackSize;
+            }
+        }
+    }
+
+    private List<Item> m_Items = new List<Item>();
+
+    public void UseItem(FighterData aUser, ScriptableObject aItemObject)
+    {
+        Item selectedItem = GetItem(aItemObject);
+
+        if(IsValidItem(selectedItem))
+        {
+            selectedItem.m_ItemData.Use(aUser);
+        }
+    }
+
+    private bool IsValidItem(Item aItem)
+    {
+        return aItem.m_ItemData != null;
+    }
+
+    private Item GetItem(ScriptableObject aItemObject)
+    {
+        for(int i = 0; i < m_Items.Count; i++)
+        {
+            Item item = m_Items[i];
+
+            if(item.m_ItemData == aItemObject)
+            {
+                return item;
+            }
+        }
+
+        return new Item();
+    }
 }
